bef637e9255b36642c423883aa45b20ca98bde8e
hazemsalim <haz.future@gmail.com>
2020-02-02 11:22:10 +0300

    Initial Phase

---
+++ public/precache-manifest.e53764184a8c85d6efc261e75896573d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f762f7293b39ceb49a3",
+    "url": "/static/css/main.172b1de3.chunk.css"
+  },
+  {
+    "revision": "1f762f7293b39ceb49a3",
+    "url": "/static/js/main.1f762f72.chunk.js"
+  },
+  {
+    "revision": "5312a633d650a9cce66b",
+    "url": "/static/js/1.5312a633.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "32e767da4c7f730785e4b6cf7a4a560b",
+    "url": "/index.html"
+  }
+];
